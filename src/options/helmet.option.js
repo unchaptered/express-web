98e742153535b5c679f3bf86f65be92b9c0a2299
Unchaptered <workstation19961002@gmail.com>
2022-05-06 23:14:26 +0900

    FIX helmet.option.js for projection settings

--- src/options/helmet.option.js
+++ src/options/helmet.option.js
@@ -1,3 +1,35 @@
-import helmet from 'helmet';
+import helmet, { hidePoweredBy } from 'helmet';
 
-export default getHelmet = () => helmet();
+// https://www.securecoding.com/blog/using-helmetjs/
+
+const contentSecurityPolicyOption = {
+    useDefaults: true,
+    directives: {
+        "script-src": ["'self'"],
+        "style-src": null
+    }
+};
+const expectCtOption = {
+    maxAge: 96400,
+    enforce: true,
+    reportUri: 'nope'
+}
+const dnsPrefetchControlOption = { allow: true };
+const rameguardOption = { action: "deny" };
+const hstsOption = { maxAge:123456, includeSubDomains: false };
+const referrerPolicyOption = { policy: ["origin", "unsafe-url"] };
+
+const getHelmet = () => helmet({
+    contentSecurityPolicy: contentSecurityPolicyOption, // Content-Security-Policy
+    dnsPrefetchControl: dnsPrefetchControlOption,       // X-DNS-Prefetch-Control
+    expectCt: expectCtOption,                           // Expect-CT
+    frameguard: rameguardOption,                        // X-Frame-Options
+    hsts: hstsOption,                                   // Strict-Transport-Security
+    referrerPolicy: referrerPolicyOption,               // Referrer-Policy
+    hidePoweredBy: true,                                // X-Powerd-By
+    ieNoOpen: true,                                     // X-Download_options
+    noSniff: true,                                      // X-Content-Type-Options
+    xssFilter: true,                                    // X-XSS-Protection
+});
+
+export default getHelmet;
