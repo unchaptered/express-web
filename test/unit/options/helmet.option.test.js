e61ed157366118453071c3573d7bc3a90bb97bf2
Unchaptered <workstation19961002@gmail.com>
2022-05-08 15:03:34 +0900

    ADD/test helmet.option.test.js

---
+++ test/unit/options/helmet.option.test.js
@@ -0,0 +1,23 @@
+import {
+    contentSecurityPolicyOption,
+    expectOption,
+    dnsPrefetchControlOption,
+    rameguardOption,
+    hstsOption,
+    referrerPolicyOption,
+    getHelmet
+} from '../../../src/options/helmet.option';
+
+describe ('Helmet', () => {
+
+    it ('getHelmet must be function', () => expect(typeof getHelmet).toBe('function'));
+    it('6 kind of option must be object', () => {
+        expect(typeof contentSecurityPolicyOption).toBe('object');
+        expect(typeof expectOption).toBe('object');
+        expect(typeof dnsPrefetchControlOption).toBe('object');
+        expect(typeof rameguardOption).toBe('object');
+        expect(typeof hstsOption).toBe('object');
+        expect(typeof referrerPolicyOption).toBe('object');
+    });
+
+});
