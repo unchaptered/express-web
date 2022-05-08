b0e63ed691a45c199ad2116da01b13377dab0d0f
Unchaptered <workstation19961002@gmail.com>
2022-05-08 15:02:48 +0900

    FIX helmet.option.js to exports for test

--- src/options/helmet.option.js
+++ src/options/helmet.option.js
@@ -1,28 +1,27 @@
 import helmet, { hidePoweredBy } from 'helmet';
 
 // https://www.securecoding.com/blog/using-helmetjs/
-
-const contentSecurityPolicyOption = {
+export const contentSecurityPolicyOption = {
     useDefaults: true,
     directives: {
         "script-src": ["'self'"],
         "style-src": null
     }
 };
-const expectCtOption = {
+export const expectOption = {
     maxAge: 96400,
     enforce: true,
     reportUri: 'nope'
 }
-const dnsPrefetchControlOption = { allow: true };
-const rameguardOption = { action: "deny" };
-const hstsOption = { maxAge:123456, includeSubDomains: false };
-const referrerPolicyOption = { policy: ["origin", "unsafe-url"] };
+export const dnsPrefetchControlOption = { allow: true };
+export const rameguardOption = { action: "deny" };
+export const hstsOption = { maxAge:123456, includeSubDomains: false };
+export const referrerPolicyOption = { policy: ["origin", "unsafe-url"] };
 
-const getHelmet = () => helmet({
+export const getHelmet = () => helmet({
     contentSecurityPolicy: contentSecurityPolicyOption, // Content-Security-Policy
     dnsPrefetchControl: dnsPrefetchControlOption,       // X-DNS-Prefetch-Control
-    expectCt: expectCtOption,                           // Expect-CT
+    expectCt: expectOption,                           // Expect-CT
     frameguard: rameguardOption,                        // X-Frame-Options
     hsts: hstsOption,                                   // Strict-Transport-Security
     referrerPolicy: referrerPolicyOption,               // Referrer-Policy
@@ -31,5 +30,3 @@ const getHelmet = () => helmet({
     noSniff: true,                                      // X-Content-Type-Options
     xssFilter: true,                                    // X-XSS-Protection
 });
-
-export default getHelmet;
